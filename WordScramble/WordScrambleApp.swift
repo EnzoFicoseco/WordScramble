2b5f52804fb1cf9363aa456e50fd3deb08355ad2
EnzoFicoseco <enzoficoseco2704@gmail.com>
2024-07-26 16:03:58 -0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Enzo Ficoseco on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
